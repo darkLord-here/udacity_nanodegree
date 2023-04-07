ee548310c25174c2628e90fa6ce5b2e96dafc5d5
darkLord-here <stupidnspooky.lord@gmail.com>
2023-04-07 16:06:39 +0530

    Project 2: completed

--- LandingPage/js/app.js
+++ LandingPage/js/app.js
@@ -1,68 +1,61 @@
-/**
- * 
- * Manipulating the DOM exercise.
- * Exercise programmatically builds navigation,
- * scrolls to anchors from navigation,
- * and highlights section in viewport upon scrolling.
- * 
- * Dependencies: None
- * 
- * JS Version: ES2015/ES6
- * 
- * JS Standard: ESlint
- * 
-*/
-
-/**
- * Comments should be present at the beginning of each procedure and class.
- * Great to have comments before crucial code sections within the procedure.
-*/
-
-/**
- * Define Global Variables
- * 
-*/
-nav = document.getElementById('navbar__list');
-sections = document.getElementsByTagName('section');
+const nav = document.getElementById('navbar__list');
+const sections = document.getElementsByTagName('section');
+let sectionPositions = []
 
 /**
  * End Global Variables
  * Start Helper Functions
  * 
 */
-navlinks = ''
-for(section of sections){
-    navlinks += `<li class="menu__link"><a href="#${section.id}">${section.dataset.nav}</a></li>`;
+function scrollOffset(x, y){
+    window.scrollTo(x, y);
 }
 
+// Create DocumentFragment to avoid multiple repaint and reflows
+let tempDocument = new DocumentFragment();
 
-/**
- * End Helper Functions
- * Begin Main Functions
- * 
-*/
+for(let [index,section] of Array.from(sections).entries()){
+    // get data-nav attribute from the section as it contains the section name
+    let name = section.getAttribute('data-nav');
+    // get the area of the section
+    let offset = section.getBoundingClientRect();
 
-// build the nav
-nav.innerHTML = navlinks
-nav.style.color = 'black';
+    // Create element and assign required attributes
+    let tag = document.createElement('li');
+    tag.setAttribute("id", `menu_link${index+1}`);
+    tag.setAttribute("class", "menu_link");
+
+    tag.style.color = 'black';
+    tag.innerHTML = `<a onClick="scrollOffset(${offset.x}, ${offset.y})">${name}</a>`
+    
+    // Add the element to Document Fragment
+    tempDocument.appendChild(tag);
+}
+
+// Append DocumentFragment as child to nav
+nav.appendChild(tempDocument);
 
 
 // Add class 'active' to section when near top of viewport
 
 
 // Scroll to anchor ID using scrollTO event
+document.addEventListener('scroll', function(event){
+    sectionPositions = []
+    Array.from(sections).forEach((section)=> sectionPositions.push(section.getBoundingClientRect().top)+30);
+    let idx = sectionPositions.findIndex((element) => element > 0);
+    console.log(idx)
+    for (let i = 0; i < sections.length; i++) {
+		if (idx === i) {
+			document.querySelector(`#menu_link${idx + 1}`).classList.add("active");
+			document.querySelector(`#section${idx + 1}`).classList.add("your-active-class");
+		} else {
+			document.querySelector(`#menu_link${i + 1}`).removeAttribute("class");
+			document.querySelector(`#section${i + 1}`).removeAttribute("class");
+		}
+	}
+});
 
 
-/**
- * End Main Functions
- * Begin Events
- * 
-*/
-
-// Build menu 
-
-// Scroll to section on link click
-
-// Set sections as active
 
 
